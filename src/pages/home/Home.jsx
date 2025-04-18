0e1c4e722df744eab06d8a068b2709766f07ffa5
Miminoshvili Temo <temurimi@gmail.com>
2025-04-18 19:20:11 +0400

    test husky

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,8 @@ import { useTranslation } from 'react-i18next'
 import './Home.scss'
 
 export default function Home() {
-  const { t } = useTranslation()
-  return <h1>{t('welcome')}</h1>
-}
+  -  const { t } = useTranslation()
+  -  return <h1>{t('welcome')}</h1>
+  +const {t}=useTranslation();return<h1>{t("welcome")}</h1>
+  }
+  
